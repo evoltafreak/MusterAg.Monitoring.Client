0f8f72023936629d8a0d9135513b70435d8fb699
Evoltafreak <joshua.sallmann@hotmail.com>
2020-06-08 19:49:55 +0200

    fix division by zero error

--- Database/v_UsagePerLocation.sql
+++ Database/v_UsagePerLocation.sql
@@ -1,8 +1,24 @@
-CREATE OR REPLACE VIEW v_UsagePerLocation
-AS
-SELECT d.idDevice, l.address, l.addressNr, l.fidPlace, d.description, d.maxcapacity, ((Select count(*) FROM networkinterface n WHERE n.fidDevice = d.idDevice)* 100) as occSlots 
-FROM location l INNER JOIN device d ON l.idLocation = d.fidLocation
-INNER JOIN categories c ON d.fidCategories = c.idCategories
-INNER JOIN networkinterface n ON d.idDevice = n.fidDevice
-GROUP BY d.idDevice;
+CREATE OR REPLACE VIEW v_UsagePerLocation AS
+    SELECT
+    d.idDevice,
+    l.address,
+    l.addressNr,
+    l.fidPlace,
+    d.description,
+    d.maxcapacity,
+    (
+        (
+            SELECT COUNT(*)
+            FROM networkinterface n
+            WHERE n.fidDevice = d.idDevice
+        ) * 100 / (CASE WHEN d.maxcapacity = 0 THEN 1 ELSE d.maxcapacity END)
+    ) AS occSlots 
+    FROM location l
+    INNER JOIN device d
+    ON l.idLocation = d.fidLocation
+    INNER JOIN categories c
+    ON d.fidCategories = c.idCategories
+    INNER JOIN networkinterface n
+    ON d.idDevice = n.fidDevice
+    GROUP BY d.idDevice;
 
--- Database/v_UsagePerPod.sql
+++ Database/v_UsagePerPod.sql
@@ -1,10 +1,29 @@
-CREATE OR REPLACE VIEW v_UsagePerPod
-AS
-SELECT d.idDevice, cu.firstname, cu.lastname, p.description as POD, d.description as Device, c.Description as Category, d.maxcapacity, ((Select count(*) FROM networkinterface n WHERE n.fidDevice = d.idDevice)* 100) as occSlots 
-FROM customer_pod cp INNER JOIN device d ON cp.fidpod = d.fidpod
-INNER JOIN pod p ON p.idPod = cp.fidPod
-INNER JOIN Customer cu ON cu.idCustomer = cp.fidCustomer
-INNER JOIN categories c ON d.fidCategories = c.idCategories
-INNER JOIN networkinterface n ON d.idDevice = n.fidDevice
-GROUP BY d.idDevice;
+CREATE OR REPLACE VIEW v_UsagePerPod AS
+    SELECT
+    d.idDevice,
+    cu.firstname,
+    cu.lastname,
+    p.description AS POD,
+    d.description AS Device,
+    c.Description AS Category,
+    d.maxcapacity,
+    (
+        (
+            SELECT COUNT(*)
+            FROM networkinterface n
+            WHERE n.fidDevice = d.idDevice
+        ) * 100 / (CASE WHEN d.maxcapacity = 0 THEN 1 ELSE d.maxcapacity END)
+    ) AS occSlots 
+    FROM customer_pod cp
+    INNER JOIN device d
+    ON cp.fidpod = d.fidpod
+    INNER JOIN pod p
+    ON p.idPod = cp.fidPod
+    INNER JOIN Customer cu
+    ON cu.idCustomer = cp.fidCustomer
+    INNER JOIN categories c
+    ON d.fidCategories = c.idCategories
+    INNER JOIN networkinterface n
+    ON d.idDevice = n.fidDevice
+    GROUP BY d.idDevice;
 
